78cc35108b4009cafb46c5d90da5f0eefe3fc68a
queiroga.gab <gabriela.queiroga1603@gmail.com>
2026-03-25 11:06:43 -0300

    Add files via upload

---
+++ src/test/java/br/com/fiap/espb/restaurant/orders/restaurant_orders/RestaurantOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.espb.restaurant.orders.restaurant_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
